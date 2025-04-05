a21093668aa5549d2633995e794288166c79f1b2
ruri <kyh693425@gmail.com>
2025-04-05 15:37:46 +0900

    refactor(hooks): create useTodos hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,18 @@
-import {useState} from 'react'
-import {useEffect} from "react";
-import {Todo} from "./types/todo";
+import {useTodos} from "./hooks/useTodos";
 import TodoInput from "./components/TodoInput.tsx";
 import TodoList from "./components/TodoList.tsx";
 
 function App() {
-    const [todos, setTodos] = useState<Todo[]>(() => {
-        const stored = localStorage.getItem("todos");
-        return stored ? JSON.parse(stored) : [];
-    });
-    const [input, setInput] = useState("");
-    const [filter, setFilter] = useState<"all" | "active" | "completed">("all");
-
-    useEffect(() => {
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }, [todos]);
-
-    const handleAddTodo = () => {
-        if (input.trim() === "") return;
-
-        const newTodo: Todo = {
-            id: Date.now(),
-            text: input,
-            completed: false,
-        };
-
-        setTodos([...todos, newTodo]);
-        setInput("");
-    };
-
-    const filteredTodos = todos.filter((todo) => {
-        if (filter === "active") return !todo.completed;
-        if (filter === "completed") return todo.completed;
-        return true; // all
-    });
-
-    const handleToggle = (id: number) => {
-        setTodos(
-            todos.map((todo) =>
-                todo.id === id ? {...todo, completed: !todo.completed} : todo
-            )
-        );
-
-        if (filter !== "all") {
-            setFilter("all");
-        }
-    };
-
-    const handleDelete = (id: number) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    };
+    const {
+        input,
+        filter,
+        setInput,
+        setFilter,
+        handleAddTodo,
+        handleToggle,
+        handleDelete,
+        filteredTodos,
+    } = useTodos();
 
     return (
         <div style={{padding: "20px", maxWidth: "400px", margin: "0 auto"}}>
