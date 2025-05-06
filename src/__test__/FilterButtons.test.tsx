67a1b52dff0580c89f442b63e331792ccdc5cf30
ruri <kyh693425@gmail.com>
2025-05-06 15:42:06 +0900

    test(FilterButtons): ensure rendering, style and click interactions

--- src/__test__/FilterButtons.test.tsx
+++ src/__test__/FilterButtons.test.tsx
@@ -1,5 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import FilterButtons from "../components/FilterButtons";
+import userEvent from "@testing-library/user-event";
+import {vi} from "vitest";
 
 describe("FilterButtons", () => {
     it("renders all filter buttons", async () => {
@@ -15,19 +17,46 @@ describe("FilterButtons", () => {
 
         const allFilter = screen.getByText("전체");
         expect(allFilter).toHaveStyle("font-weight: bold");
+        const activeFilter = screen.getByText("진행중");
+        expect(activeFilter).not.toHaveStyle("font-weight: bold");
+        const completedFilter = screen.getByText("완료");
+        expect(completedFilter).not.toHaveStyle("font-weight: bold");
     });
 
     it("highlights selected active filter button", async () => {
         render(<FilterButtons filter="active" setFilter={() => {}}/>);
 
+        const allFilter = screen.getByText("전체");
+        expect(allFilter).not.toHaveStyle("font-weight: bold");
         const activeFilter = screen.getByText("진행중");
         expect(activeFilter).toHaveStyle("font-weight: bold");
+        const completedFilter = screen.getByText("완료");
+        expect(completedFilter).not.toHaveStyle("font-weight: bold");
     });
 
     it("highlights selected completed filter button", async () => {
         render(<FilterButtons filter="completed" setFilter={() => {}}/>);
 
+        const allFilter = screen.getByText("전체");
+        expect(allFilter).not.toHaveStyle("font-weight: bold");
+        const activeFilter = screen.getByText("진행중");
+        expect(activeFilter).not.toHaveStyle("font-weight: bold");
         const completedFilter = screen.getByText("완료");
         expect(completedFilter).toHaveStyle("font-weight: bold");
+    });
+
+    it("calls setFilter when each button is clicked", async () => {
+        const dummyFilter = "all";
+        const mockSetFilter = vi.fn();
+        render(<FilterButtons filter={dummyFilter} setFilter={mockSetFilter} />);
+
+        await userEvent.click(screen.getByText("전체"));
+        expect(mockSetFilter).toHaveBeenCalledWith("all");
+
+        await userEvent.click(screen.getByText("진행중"));
+        expect(mockSetFilter).toHaveBeenCalledWith("active");
+
+        await userEvent.click(screen.getByText("완료"));
+        expect(mockSetFilter).toHaveBeenCalledWith("completed");
     });
 });
